ee4af8671a37001231238e6b8e9b8ca61e51b728
chu700 <104961710+chu700@users.noreply.github.com>
2024-10-22 11:05:23 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,26 +1,29 @@
 jQuery(document).ready(function () {
-    $('.navi>li').mouseover(function () {
-        $(this).find('.submenu').stop().slideDown(500);
-    }).mouseout(function () {
-        $(this).find('.submenu').stop().slideUp(500);
-    });
 
-    setInterval(function () {
-        $('.slidelist').delay(1000);
-        $('.slidelist').animate({ marginTop: -300 });
-        $('.slidelist').delay(2000);
-        $('.slidelist').animate({ marginTop: -600 });
-        $('.slidelist').delay(2000);
-        $('.slidelist').animate({ marginTop: 0 });
-        $('.slidelist').delay(2000);
-    });
+  $('.navi>li').mouseover(function () {
+    $('.submenu').stop().slideDown(500);
+    $("#menu_bg").stop().slideDown(500);
+  }).mouseout(function () {
+    $('.submenu').stop().slideUp(500);
+    $("#menu_bg").stop().slideUp(500);
+  });
 
-    $(function () {
-        $('.tabmenu>li>a').click(function () {
-            $(this).parent().addClass("active")
-                .siblings()
-                .removeClass("active");
-            return false;
-        });
-    });
+  $('.imgslide a:gt(0)').hide();
+  setInterval(function () {
+    $('.imgslide a:first-child')
+      .fadeOut()
+      .next('a')
+      .fadeIn()
+      .end()
+      .appendTo('.imgslide');
+  }, 3000);
+
+  $(".notice li:nth-child(2)").click(function () {
+    $("#modal").addClass("active");
+  });
+  $(".btn").click(function () {
+    $("#modal").removeClass("active");
+  });
+
 });
+
